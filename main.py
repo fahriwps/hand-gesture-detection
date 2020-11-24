68c4cb7419f27c21556593f75ed4018db729be55
Fahri <68985644+fahriwps@users.noreply.github.com>
2020-11-24 22:30:46 +0700

    update main.py

--- main.py
+++ main.py
@@ -5,65 +5,61 @@ from sklearn.metrics import pairwise
 
 bg = None
 
-#Fungsi melakukan metode rerata frame dan substraction
+# Averaging frame and substraction
 def run_avg(image, aWeight):
     global bg
-    #Mencari frame background
+    # Extracting frame background
     if bg is None:
         bg = image.copy().astype("float")
         return
 
-    #melakukan kalkulasi antara frame background dengan frame berobjek baru
+    # Calculating background frame againsts new frame with new objects
     cv2.accumulateWeighted(image, bg, aWeight)
 
-#Fungsi melakukan segmentasi dan thresholding pada objek tangan
+# Segmentation and thresholding hand object
 def segment(image, threshold=25):
     global bg
-    # mencari perbedaan antara frame bg dengan frame baru
+    # comparing difference background frame againsts new frame
     diff = cv2.absdiff(bg.astype("uint8"), image)
 
-    # melakukan thresholding pada objek baru
+    # thresholding new objects
     thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
 
-    # didapatkan kontur dari proses thresholding 
+    # calculating contour from thresholding 
     (_, cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if len(cnts) == 0:
         return
     else:
-        # mendapatkan kontur tangan
+        # getting contour
         segmented = max(cnts, key=cv2.contourArea)
         return (thresholded, segmented)
 
 
-# Fungsi menghitung banyak jari yang muncul pada kontur tangan
+# Calculating how many fingers on contour
 def count(thresholded, segmented):
-	# mencari convex hull dari hasi segmentasi tangan
 	chull = cv2.convexHull(segmented)
-
-	# mencari titik ekstrim dari convex hull
+	
 	extreme_top    = tuple(chull[chull[:, :, 1].argmin()][0])
 	extreme_bottom = tuple(chull[chull[:, :, 1].argmax()][0])
 	extreme_left   = tuple(chull[chull[:, :, 0].argmin()][0])
 	extreme_right  = tuple(chull[chull[:, :, 0].argmax()][0])
 
-	# mencari titik tengah dari tangan
 	cX = (extreme_left[0] + extreme_right[0]) // 2
 	cY = (extreme_top[1] + extreme_bottom[1]) // 2
 
-	# mencari nilai euclidean maksimum dari titik tengah tangan dan titik ekstrim 
 	distance = pairwise.euclidean_distances([(cX, cY)], Y=[extreme_left, extreme_right, extreme_top, extreme_bottom])[0]
 	maximum_distance = distance[distance.argmax()]
 	
-	# melakukan kalkulasi radius lingkaran dari 80% nilai maksimum euclidean 
+	# calculating circle radius 
 	radius = int(0.8 * maximum_distance)
 	circumference = (2 * np.pi * radius)
 	circular_roi = np.zeros(thresholded.shape[:2], dtype="uint8")
 
-	# menggambar lingkaran sesuai radius
+	# drawing cirle from new radius
 	cv2.circle(circular_roi, (cX, cY), radius, 255, 1)
 	
-	# melakukan operasi bitwise AND pada lingkaran
+	# bitwise AND on circle
 	circular_roi = cv2.bitwise_and(thresholded, thresholded, mask=circular_roi)
 	(_, cnts, _) = cv2.findContours(circular_roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 	count = 0 
@@ -74,7 +70,6 @@ def count(thresholded, segmented):
 			count += 1
 	return count
 
-#Fungsi utama
 if __name__ == "__main__":
     accumWeight = 0.5
     camera = cv2.VideoCapture(0)
